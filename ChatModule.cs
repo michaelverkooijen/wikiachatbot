a34b9dfd0b47d5db4ae1e63839fcd8b5cf0dea2b
Michael Verkooijen <michaelverkooijen@gmail.com>
2014-09-05 01:51:17 +0200

    fixed version, TODO: update speak method

--- ChatModule.cs
+++ ChatModule.cs
@@ -15,7 +15,17 @@ namespace WikiaBot {
 	public class ChatModule {
 		private string wiki, user, pass, youtubeCredentials, chatKey, sid, nodeHost;
 		//TODO: replace with settings.json
-		private string[] patterns = {"fu?ck", "f[a@]g", "cunt", "wanker", "d[i!]ck", "nigg[ae]r?", "slut", "wh[o0]re", "c[o0]ck"};
+		private string[] patterns = {
+			"fu?ck",
+			"f[a@]g",
+			"cunt",
+			"wanker",
+			"d[i!]ck",
+			"nigg[ae]r?",
+			"slut",
+			"wh[o0]re",
+			"c[o0]ck"
+		};
 		private int roomId, nodeInstance;
 		ConnectionManager cm;
 		ArrayList namesBlacklist;
@@ -67,7 +77,7 @@ namespace WikiaBot {
 						"name=" + user,
 						"key=" + chatKey,
 						"roomId=" + roomId.ToString (),
-						"serverId=" + nodeInstance.ToString(),
+						"serverId=" + nodeInstance.ToString (),
 						"EIO=2",
 						"transport=polling",
 						//"t=" + (DateTime.Now.ToUniversalTime () - new DateTime (1970, 1, 1)).TotalSeconds,
@@ -133,65 +143,61 @@ namespace WikiaBot {
 		private bool parseResponse (string s) {
 			int prefix = s.IndexOf ("\"");
 			prefix--;
-			Console.WriteLine(prefix); //4 etc
+			Console.WriteLine (prefix); //4 etc
 			if (prefix > 0) {
 				s = s.Substring (prefix, s.Length - prefix);
 				//Console.WriteLine(s); full string without 4::
+				Console.WriteLine ("Stripped string: " + s);
 				var token = JToken.Parse (s);
 				if (token is JArray) {
-					s = (string)token.Last;
-					//var o = JArray.Parse (s); //second element contains expected JObject
-				}
-				var o = JObject.Parse (s);
-				string eve = (string)o ["event"];
-				//Console.WriteLine(eve);
-				switch (eve) {
-				case "sid":
-					sid = JObject.Parse ((string)o ["sid"]).ToString ();
-					Console.WriteLine ("new sid: " + sid);
-					break;
-				case "chat:add":
-					{
-						var data = JObject.Parse ((string)o ["data"]);
-						string name = (string)data ["attrs"] ["name"];
-						//Console.WriteLine("Name: " + name);
-						string text = (string)data ["attrs"] ["text"];
-						//Console.WriteLine("Text: " + text);
-						string timestamp = (string)data ["attrs"] ["timeStamp"];
-						//Console.WriteLine(timestamp);
-						//var dt = new DateTime (1970, 1, 1, 0, 0, 0, 0).AddSeconds (Math.Round (Convert.ToInt64 (timestamp) / 1000d)).ToLocalTime ();
-						var dt = new DateTime (1970, 1, 1, 0, 0, 0, 0).AddSeconds (Math.Round (Convert.ToInt64 (timestamp) / 1000d)).ToString ("yyyyMMdd HH:mm:ss");
-						string line = "*" + dt.Split (' ') [1] + ": [[User:" + name + "|]]: <nowiki>" + text + "</nowiki>";
-						Console.WriteLine (line);
-						string filename = dt.Split (' ') [0] + ".log";
-						Console.WriteLine (filename);
-						try {
-							using (StreamWriter file = File.AppendText (@filename)) {
-								file.WriteLine (line);
+					Console.WriteLine ("JArray detected!");
+					s = (string)token.Last.ToString();
+					var o = JObject.Parse (s);
+					string eve = (string)o ["event"];
+					Console.WriteLine (eve);
+					switch (eve) {
+					case "chat:add":
+						{
+							var data = JObject.Parse ((string)o ["data"]);
+							string name = (string)data ["attrs"] ["name"];
+							//Console.WriteLine("Name: " + name);
+							string text = (string)data ["attrs"] ["text"];
+							//Console.WriteLine("Text: " + text);
+							string timestamp = (string)data ["attrs"] ["timeStamp"];
+							//Console.WriteLine(timestamp);
+							//var dt = new DateTime (1970, 1, 1, 0, 0, 0, 0).AddSeconds (Math.Round (Convert.ToInt64 (timestamp) / 1000d)).ToLocalTime ();
+							var dt = new DateTime (1970, 1, 1, 0, 0, 0, 0).AddSeconds (Math.Round (Convert.ToInt64 (timestamp) / 1000d)).ToString ("yyyyMMdd HH:mm:ss");
+							string line = "*" + dt.Split (' ') [1] + ": [[User:" + name + "|]]: <nowiki>" + text + "</nowiki>";
+							Console.WriteLine (line);
+							string filename = dt.Split (' ') [0] + ".log";
+							Console.WriteLine (filename);
+							try {
+								using (StreamWriter file = File.AppendText (@filename)) {
+									file.WriteLine (line);
+								}
+							} catch (Exception e) {
+								Console.WriteLine (e.ToString ());
 							}
-						} catch (Exception e) {
-							Console.WriteLine (e.ToString ());
-						}
-						if (isMod && containsBadLanguage (text)) {
-							Random r = new Random ();
-							switch (r.Next (6)) {
-							case 0:
-								speak (name + ", please watch your language.");
-								break;
-							case 1:
-								speak ("Please refrain from using bad words");
-								break;
+							if (isMod && containsBadLanguage (text)) {
+								Random r = new Random ();
+								switch (r.Next (6)) {
+								case 0:
+									speak (name + ", please watch your language.");
+									break;
+								case 1:
+									speak ("Please refrain from using bad words");
+									break;
+								}
 							}
-						}
-						if (name.Equals("Flightmare") && text.Equals("!tm")) { 
-							isMod = !isMod;
-							speak ("Moderate chat: " + isMod.ToString());
-						}
-						if (name.Equals("Flightmare") && text.Equals("!tw")) { 
-							doesWelcome = !doesWelcome;
-							speak ("Welcome users: " + doesWelcome.ToString());
-						}
-						/*if (text.Contains ("youtube")) { //TODO: regex: youtu.?be
+							if (name.Equals ("Flightmare") && text.Equals ("!tm")) { 
+								isMod = !isMod;
+								speak ("Moderate chat: " + isMod.ToString ());
+							}
+							if (name.Equals ("Flightmare") && text.Equals ("!tw")) { 
+								doesWelcome = !doesWelcome;
+								speak ("Welcome users: " + doesWelcome.ToString ());
+							}
+							/*if (text.Contains ("youtube")) { //TODO: regex: youtu.?be
 						string[] words = text.Split(' ');
 						foreach (string word in words){
 							string[] args = word.Split ('?');
@@ -211,64 +217,70 @@ namespace WikiaBot {
 							}
 						}
 					}*/
-					}
-					break;
-				case "join":
-					{
-						if (doesWelcome) {
-							var data = JObject.Parse ((string)o ["data"]);
-							string name = (string)data ["attrs"] ["name"];
-							if (!namesBlacklist.Contains (name)) {
-								speak ("Hello there, " + name + "!");
-								namesBlacklist.Add (name);
+						}
+						break;
+					case "join":
+						{
+							if (doesWelcome) {
+								var data = JObject.Parse ((string)o ["data"]);
+								string name = (string)data ["attrs"] ["name"];
+								if (!namesBlacklist.Contains (name)) {
+									speak ("Hello there, " + name + "!");
+									namesBlacklist.Add (name);
+								}
 							}
 						}
-					}
-					break;
+						break;
 					//case "part" //TODO: check json, remove user from warnlist
-				case "kick":
-					{
-						var data = JObject.Parse ((string)o ["data"]);
-						string kickedName = (string)data ["attrs"] ["kickedUserName"];
-						string modName = (string)data ["attrs"] ["moderatorName"];
-						//string reason = (string)data ["attrs"] ["reason"];
-						//TODO: test for correct timezone settings (should be UTC)
-						string filename = DateTime.Now.ToUniversalTime ().ToString ("yyyyMMdd") + ".log";
-						string line = "**[[User:" + modName + "|]] kicked [[User:" + kickedName + "|]]";
-						Console.WriteLine (line);
-						Console.WriteLine (DateTime.Now.ToUniversalTime ().ToString ("HH:mm:ss"));
-						Console.WriteLine ("kick-filename: "+filename);
-						try {
-							using (StreamWriter file = File.AppendText (@filename)) {
-								file.WriteLine (line);
+					case "kick":
+						{
+							var data = JObject.Parse ((string)o ["data"]);
+							string kickedName = (string)data ["attrs"] ["kickedUserName"];
+							string modName = (string)data ["attrs"] ["moderatorName"];
+							//string reason = (string)data ["attrs"] ["reason"];
+							//TODO: test for correct timezone settings (should be UTC)
+							string filename = DateTime.Now.ToUniversalTime ().ToString ("yyyyMMdd") + ".log";
+							string line = "**[[User:" + modName + "|]] kicked [[User:" + kickedName + "|]]";
+							Console.WriteLine (line);
+							Console.WriteLine (DateTime.Now.ToUniversalTime ().ToString ("HH:mm:ss"));
+							Console.WriteLine ("kick-filename: " + filename);
+							try {
+								using (StreamWriter file = File.AppendText (@filename)) {
+									file.WriteLine (line);
+								}
+							} catch (Exception e) {
+								Console.WriteLine (e.ToString ());
 							}
-						} catch (Exception e) {
-							Console.WriteLine (e.ToString ());
 						}
-					}
-					break;
-				case "ban":
-					{
-						var data = JObject.Parse ((string)o ["data"]);
-						string kickedName = (string)data ["attrs"] ["kickedUserName"];
-						string modName = (string)data ["attrs"] ["moderatorName"];
-						string reason = (string)data ["attrs"] ["reason"];
-						//int time = (int)data ["attrs"] ["time"];
-						//TODO: test for correct timezone settings (should be UTC)
-						string filename = DateTime.Now.ToUniversalTime ().ToString ("yyyyMMdd") + ".log";
-						string line = "**[[User:" + modName + "|]] banned [[User:" + kickedName + "|]] with reason: " + reason;
-						Console.WriteLine (line);
-						Console.WriteLine (DateTime.Now.ToUniversalTime ().ToString ("HH:mm:ss"));
-						Console.WriteLine ("kick-filename: "+filename);
-						try {
-							using (StreamWriter file = File.AppendText (@filename)) {
-								file.WriteLine (line);
+						break;
+					case "ban":
+						{
+							var data = JObject.Parse ((string)o ["data"]);
+							string kickedName = (string)data ["attrs"] ["kickedUserName"];
+							string modName = (string)data ["attrs"] ["moderatorName"];
+							string reason = (string)data ["attrs"] ["reason"];
+							//int time = (int)data ["attrs"] ["time"];
+							//TODO: test for correct timezone settings (should be UTC)
+							string filename = DateTime.Now.ToUniversalTime ().ToString ("yyyyMMdd") + ".log";
+							string line = "**[[User:" + modName + "|]] banned [[User:" + kickedName + "|]] with reason: " + reason;
+							Console.WriteLine (line);
+							Console.WriteLine (DateTime.Now.ToUniversalTime ().ToString ("HH:mm:ss"));
+							Console.WriteLine ("kick-filename: " + filename);
+							try {
+								using (StreamWriter file = File.AppendText (@filename)) {
+									file.WriteLine (line);
+								}
+							} catch (Exception e) {
+								Console.WriteLine (e.ToString ());
 							}
-						} catch (Exception e) {
-							Console.WriteLine (e.ToString ());
 						}
+						break;
 					}
-					break;
+				} else {
+					Console.WriteLine ("No JArray here");
+					var o = JObject.Parse (s);
+					sid = (string)o ["sid"];
+					Console.WriteLine ("new sid: " + sid);
 				}
 			}
 			return true;
@@ -280,7 +292,7 @@ namespace WikiaBot {
 				"name=" + user,
 				"key=" + chatKey,
 				"roomId=" + roomId.ToString (),
-				"serverId=" + nodeHost.ToString(),
+				"serverId=" + nodeHost.ToString (),
 				"EIO=2",
 				"transport=polling",
 				"t=" + (DateTime.Now.ToUniversalTime () - new DateTime (1970, 1, 1)).TotalSeconds
